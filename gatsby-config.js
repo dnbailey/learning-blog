c3301d78338c913615c1c47a2e4b31faf024697b
Nathan Bailey <dbailey@k12k.com>
2020-03-12 21:08:31 -0400

    Add plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  siteMetadata: {
+    title: "Never Stop Learning",
+    author: "Nathan Bailey",
+    description: "A learning journal...",
+  },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
@@ -7,6 +12,45 @@ module.exports = {
         path: `${__dirname}/posts`,
       },
     },
-    `gatsby-transformer-remark`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1100,
+              withWebp: true,
+              linkImagesToOriginal: false,
+              showCaptions: true,
+            },
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-react-head`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Montserrat`,
+            variants: [`400`],
+          },
+          {
+            family: `Lato`,
+            variants: [`300`],
+          },
+        ],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-html-attributes",
+      options: {
+        lang: "en",
+      },
+    },
   ],
 }
